72f371b116a34f6ee8c653033492ae3234aa628e
Konstantin Grenaderov <fkg002c@gmail.com>
2026-06-02 23:25:54 +0300

    Lesson 6: Modifiers and Arguments of Composable Functions

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.fkg002c.composeapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
     }
 
     buildTypes {
--- composeapp/src/main/java/com/fkg002c/composeapp/MainActivity.kt
+++ composeapp/src/main/java/com/fkg002c/composeapp/MainActivity.kt
@@ -4,25 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.fkg002c.composeapp.ui.theme.mainBlackColor
+import com.fkg002c.composeapp.ui.theme.subtitleGrayColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,17 +46,22 @@ class MainActivity : ComponentActivity() {
                 content = { padding: PaddingValues ->
                     Column(
                         modifier = Modifier
+                            // .fillMax...
+                            // .background
+                            // . clip
+                            // .alpha
+                            // .rotate
                             .padding(padding)
-                            .background(Color.LightGray)
-//                            .fillMaxSize()
-                            .width(200.dp)
-                            .height(200.dp),
+//                            .background(Color.LightGray)
+                            .fillMaxSize(),
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center
                     ) {
                         StudyAppHeader()
                         Spacer(modifier = Modifier.height(30.dp))
                         MainNavButtons()
+                        Spacer(modifier = Modifier.height(30.dp))
+                        StartImageButton()
                     }
                 }
             )
@@ -59,11 +77,14 @@ fun StudyAppHeader() {
         Text(
             text = "AndroidSprint",
             fontSize = 28.sp,
-            //           modifier = Modifier.align(Alignment.End)
+            color = mainBlackColor,
+            fontFamily = FontFamily(Font(R.font.gilroy_semibold)),
         )
         Text(
             text = "Learning Kotlin & Android through practice",
-            fontSize = 16.sp
+            fontSize = 16.sp,
+            color = subtitleGrayColor,
+            fontFamily = FontFamily(Font(R.font.gilroy_medium)),
         )
     }
 }
@@ -75,28 +96,68 @@ fun MainNavButtons() {
     ) {
         Button(
             onClick = {},
+            shape = RoundedCornerShape(13.dp),
             modifier = Modifier
                 .weight(1f)
                 .padding(5.dp)
         ) {
-            Text("Lessons")
+            Text(
+                text = "Lessons",
+                fontFamily = FontFamily(Font(R.font.gilroy_bold)),
+            )
         }
         Button(
             onClick = {},
+            shape = RoundedCornerShape(13.dp),
             modifier = Modifier
                 .weight(1f)
                 .padding(5.dp)
         ) {
-            Text("Tests")
+            Text(
+                text = "Tests",
+                fontFamily = FontFamily(Font(R.font.gilroy_bold)),
+            )
         }
         Button(
             onClick = {},
+            shape = RoundedCornerShape(13.dp),
             modifier = Modifier
                 .weight(1f)
                 .padding(5.dp)
         ) {
-            Text("Practice")
+            Text(
+                text = "Practice",
+                fontFamily = FontFamily(Font(R.font.gilroy_bold)),
+            )
         }
+    }
+}
+
+@Composable
+fun StartImageButton() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            contentDescription = "",
+            painter = painterResource(R.drawable.rocket2),
+            modifier = Modifier
+                .size(120.dp)
+                .shadow(3.dp, CircleShape) // order!
+                .clip(CircleShape)                   // order!
+                .clickable(                          // order!
+                    onClick = {},
+                    indication = ripple(),
+                    interactionSource = remember { MutableInteractionSource() }
+                )
+        )
+        Text(
+            text = "Start onboarding",
+            fontSize = 16.sp,
+            color = subtitleGrayColor,
+            fontFamily = FontFamily(Font(R.font.gilroy_medium)),
+        )
+
     }
 }
 
@@ -110,4 +171,10 @@ private fun StudyAppHeaderPreview() {
 @Preview
 fun MainNavButtonsPreview() {
     MainNavButtons()
+}
+
+@Composable
+@Preview
+fun StartImageButtonPreview(modifier: Modifier = Modifier) {
+    StartImageButton()
 }
